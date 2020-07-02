b4a264c97fe2b8fecbb7b80bdc34532f0f886d34
Alexander Infante <alexinfante17@gmail.com>
2020-07-02 10:34:40 -0700

    Incorporating nextjs-images dependency for better load time of images for profile picture

--- components/layout.js
+++ components/layout.js
@@ -39,7 +39,7 @@ export default function Layout({ children, home }) {
             <Link href="/">
               <a>
                 <img
-                  src="/images/profile_Alex.jpg"
+                  src={require('../public/images/profile_Alex.jpg')}
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
